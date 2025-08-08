743541354d9b9423e7d7833b2489114030b66183
vicky <vaikuntaraojagalingi@gmail.com>
2025-08-08 23:38:14 +0530

    added

--- kubernetes-instance/docker.sh
+++ kubernetes-instance/docker.sh
@@ -20,3 +20,19 @@ lvextend -L +10G /dev/RootVG/varVol
 # Grow XFS filesystems to use the new space
 sudo xfs_growfs /
 sudo xfs_growfs /var
+
+# Install eksctl for EKS cluster creation
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+eksctl version
+
+# Install kubectl for cluster interaction
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version

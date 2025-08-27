bd9542a81c76e424486bf05504e2e02c48217675
vicky <vaikuntaraojagalingi@gmail.com>
2025-08-27 16:54:19 +0530

    added

--- k8-instance/docker.sh
+++ k8-instance/docker.sh
@@ -36,5 +36,9 @@ chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 kubectl version
 
+#helm install
+$ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+$ chmod 700 get_helm.sh
+$ ./get_helm.sh
 #just download git repo
 git clone https://github.com/vicky-1729/kubernetes.git
